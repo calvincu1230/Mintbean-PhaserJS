61b66938775aef795f07276a76e29e03d736be49
ccurnuck <calvin.curnuck@gmail.com>
2020-08-20 17:39:51 -0400

    Added game options

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,7 @@
-
-
 export const gameOptions = {
   fallSpeed: 800,
   jumpBurst: 300,
   localStorageBestScore: "BestScorePhaseJS",
-}
-
-// WILL LIKELY DELETE BELOW METHODS / VARIABLES
-//==============================================
-
-const ACCELERATION = 10;
-const MAX_SPEED = 300;
-
-export function decelerate(oldValue) {
-  const polarity = Math.sign(oldValue);
-
-  if (polarity === 0) {
-    // value was 0
-    return 0;
-  } else {
-    let newVal = (Math.abs(oldValue) - ACCELERATION);
-    if (newVal < 0) newVal = 0;
-    return polarity * newVal;
-  }
-}
-export function accelerate(oldValue, direction) {
-  const newValue = oldValue + (direction * ACCELERATION);
-  const limitFunc = direction < 0 ? Math.min : Math.min;
-  return limitFunc(newValue, MAX_SPEED);
+  objSpawnTimers: [150, 100, 50, 25],
+  objSpeeds: [150, 250]
 }
